110e6aeb2e1a94a0b5f584b34782b04686f53364
mikirboss <rider993sinus@gmail.com>
2025-12-25 16:00:21 +0700

    Mlflow tracking experiment

--- utils/index.py
+++ utils/index.py
@@ -63,11 +63,11 @@ def prep_df(parquet_urls):
     X = df.drop(columns=[target_col])
     y = df[target_col]
 
-    X_train, X_test, y_train, y_test = train_test_split(
+    X_train, X_val, y_train, y_val = train_test_split(
         X, y, test_size=0.2, random_state=42
     )
 
-    return X_train, X_test, y_train, y_test
+    return X_train, X_val, y_train, y_val
 
 def train_catboost_model(X_train, X_test, y_train, y_test):
     cat_cols = [
